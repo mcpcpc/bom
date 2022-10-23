b9cea96be8af8c2d658ff7f3fb7846eb590c252d
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-10-23 17:09:39 -0400

    add new item modal

--- pages/builder.py
+++ pages/builder.py
@@ -4,12 +4,37 @@
 import dash
 import dash_mantine_components as dmc
 
+from dash import Input
+from dash import Output
+from dash import State
+from dash import callback
+
 from dash_iconify import DashIconify
+
+modal_add_item = dmc.Modal(
+    id="modal-add-item",
+    title="Add Item",
+    centered=True,
+    children=[
+        dmc.Text(""),
+        dmc.Space(h=20),
+        dmc.Group(
+            position="right",
+            children=[
+                dmc.Button(
+                    id="add-item",
+                    children="Add Item"
+                )
+            ] 
+        )
+    ]
+)
 
 layout = dmc.Container(
     children=[
+        modal_add_item,
         dmc.Button(
-            id="add-item",
+            id="open-modal-add-item",
             children="Add Item",
             leftIcon=[DashIconify(icon="carbon:add")],
         )
@@ -23,3 +48,12 @@ dash.register_page(
     description="",
     layout=layout
 )
+
+@callback(
+    Output("modal-add-item", "opened"),
+    Input("open-modal-add-item", "n_clicks"),
+    State("modal-add-item", "opened"),
+    prevent_initial_call=True)
+def modal_add_item_update(n_clicks, opened):
+    return not opened
+    
