69bf9c77cf528d93e2ab7f3c801a93e7b12f71ac
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-10-24 07:21:25 -0400

    test

--- pages/builder.py
+++ pages/builder.py
@@ -16,9 +16,41 @@ from utils.lineitem import Unit
 from utils.lineitem import Classification
 from utils.nexar import NexarLifeCycle
 
-modal_add_item = dmc.Modal(
-    id="modal-add-item",
-    title="Add Item",
+modal_automatic_add_item = dmc.Modal(
+    id="modal-manual-add-item",
+    title="Manually Add Item",
+    centered=True,
+    children=[
+        dmc.Select(
+            id="nexar-search",
+            label="Search NEXAR Database",
+            description="Automatically populate the line item parameters based NEXAR database search results.",
+            searchable=True,
+            clearable=True,
+            placeholder="Search MPN...",
+            nothingFound="No match found",
+            icon=[
+                DashIconify(
+                    icon="radix-icons:magnifying-glass"
+                )
+            ],
+        ),
+        dmc.Space(h=20),
+        dmc.Group(
+            position="right",
+            children=[
+                dmc.Button(
+                    id="automatic-add-item",
+                    children="Add"
+                )
+            ] 
+        )
+    ]
+)
+
+modal_manual_add_item = dmc.Modal(
+    id="modal-manual-add-item",
+    title="Manually Add Item",
     centered=True,
     children=[
         dmc.Text("Inserts a new line item into the current bill of material."),
@@ -36,20 +68,6 @@ modal_add_item = dmc.Modal(
             id="child",
             label="Child",
             min=0
-        ),
-        dmc.Select(
-            id="nexar-search",
-            label="Search NEXAR Database",
-            description="Automatically populate the line item parameters based NEXAR database search results.",
-            searchable=True,
-            clearable=True,
-            placeholder="Search MPN...",
-            nothingFound="No match found",
-            icon=[
-                DashIconify(
-                    icon="radix-icons:magnifying-glass"
-                )
-            ],
         ),
         dmc.TextInput(
             id="mpn",
@@ -79,7 +97,7 @@ modal_add_item = dmc.Modal(
             position="right",
             children=[
                 dmc.Button(
-                    id="add-item",
+                    id="manual-add-item",
                     children="Add"
                 )
             ] 
@@ -89,10 +107,16 @@ modal_add_item = dmc.Modal(
 
 layout = dmc.Container(
     children=[
-        modal_add_item,
+        modal_manual_add_item,
+        modal_automatic_add_item,
         dmc.Button(
-            id="open-modal-add-item",
-            children="Add Item",
+            id="open-modal-manual-add-item",
+            children="Manual",
+            leftIcon=[DashIconify(icon="carbon:add")],
+        ),
+        dmc.Button(
+            id="open-modal-automatic-add-item",
+            children="Automatic",
             leftIcon=[DashIconify(icon="carbon:add")],
         )
     ]
@@ -107,9 +131,17 @@ dash.register_page(
 )
 
 @callback(
-    Output("modal-add-item", "opened"),
-    Input("open-modal-add-item", "n_clicks"),
-    State("modal-add-item", "opened"),
+    Output("modal-manual-add-item", "opened"),
+    Input("open-modal-manual-add-item", "n_clicks"),
+    State("modal-manual-add-item", "opened"),
     prevent_initial_call=True)
-def modal_add_item_update(n_clicks, opened):
+def modal_manualadd_item_update(n_clicks, opened):
+    return not opened
+
+ @callback(
+    Output("modal-automatic-add-item", "opened"),
+    Input("open-modal-automatic-add-item", "n_clicks"),
+    State("modal-automatic-add-item", "opened"),
+    prevent_initial_call=True)
+def modal_manualadd_item_update(n_clicks, opened):
     return not opened
